69ed6a72873fac002233cc31101bfeeb75041755
dzinghan <jing.h.sun@mail.mcgill.ca>
2021-01-27 20:22:17 -0500

    initial admin page

---
+++ client/src/pages/adminHome.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Header } from '../components/header';
+
+const AdminHome = () => {
+    return (
+        <>
+            <Header/>
+            <br></br>
+            <br></br>
+            <br></br>
+            <h1>Welcome Admin!</h1>
+        </>
+    )
+
+}
+
+export default AdminHome;
