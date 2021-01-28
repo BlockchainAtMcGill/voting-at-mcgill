86b02d14c3ca5e8e49360794a2a1cd9328281cfd
dzinghan <jing.h.sun@mail.mcgill.ca>
2021-01-27 23:15:59 -0500

    can access the new election and petition page via adminHome

--- client/src/pages/adminHome.js
+++ client/src/pages/adminHome.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link'
 import { Header } from '../components/header';
 
 const AdminHome = () => {
@@ -9,6 +10,19 @@ const AdminHome = () => {
             <br></br>
             <br></br>
             <h1>Welcome Admin!</h1>
+            <div>
+                <Link href="/newElection" key="/newElection">
+                    <button >Add a new election</button>
+                </Link>
+
+                <Link href="/newPetition" key="/newPetition">
+                    <button >Add a new petition</button>
+                </Link>
+            </div>
+            <div>
+                <button>Manage current elections or petitions</button>
+                <button>View current elections or petitions</button>
+            </div>
         </>
     )
 
