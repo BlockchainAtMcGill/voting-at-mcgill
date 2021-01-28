a168b05391f0746c772bf27415092f094447d381
dzinghan <jing.h.sun@mail.mcgill.ca>
2021-01-28 13:56:22 -0500

    initial cancel component

---
+++ client/src/components/cancel.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Cancel = () => {
+    return (
+        <>
+            <div>
+                <p>
+                    Are you sure you want to discard this?
+                    Changes will not be saved.
+                </p>
+            </div>
+        </>
+    )
+}
