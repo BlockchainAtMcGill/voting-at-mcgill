f74b8842ee1d96713c0683bf75d64ce4b02a125f
luaiabuelsamen <55265045+luaiabuelsamen@users.noreply.github.com>
2021-01-30 23:17:08 +0300

    test for candidate

--- test/candidate.js
+++ test/candidate.js
@@ -19,3 +19,38 @@
 //      })
 //    });
 //  });
+const VoteFactory = artifacts.require("./VoteFactory.sol");
+const Vote = artifacts.require("./Vote.sol");
+
+contract("Vote", accounts => {
+  let factory;
+  let electionAddress;
+  let petitionAddress;
+  let election;
+  let petition;
+  before(async() => {
+    factory = await VoteFactory.deployed();
+    await factory.createVote(0, {
+      from: accounts[0]
+    });
+    await factory.createVote(1, {
+      from: accounts[0]
+    });
+    [electionAddress] = await factory.getDeployedVotes();
+    election = await Vote.at(electionAddress);
+  });
+  describe('candidate', async () =>{
+    it('candidate can enter', async() => {
+      await election.editElection("title", 1, 3, "description", [1,2,3]);
+      const currentElection = await (election.currentElection());
+      const currentCandidate= await (election.enterElection('user', 'party1', 2 ));
+      //const currentCandidate = election.candidates[accounts[0]];
+
+      //assert.ok(election.candidates[accounts[0]]);
+    })
+    it('Existing candidate can leave', async() => {
+      await election.leaveElection(2);
+      assert.equal(election.candidates.length,0);
+      })
+    })
+  });
