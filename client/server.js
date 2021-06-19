995d89bfb98635845c08adc64800349285e38a49
SimonTheWang <simonwg2001@gmail.com>
2021-06-18 20:58:41 -0400

    added hosyt

--- client/server.js
+++ client/server.js
@@ -8,9 +8,12 @@ const app = next ({
 const routes = require('./routes');
 const handler = routes.getRequestHandler(app);
 
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+
 app.prepare().then(() => {
     // createServer(handler).listen(process.env.PORT || 3000, err => {
-    createServer(handler).listen(3000, err => {
+    createServer(handler).listen(port,host, err => {
         if (err) throw err;
         console.log("Ready on localhost:3000"); 
     })
